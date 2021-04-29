1f25cfe00b856ab927b0f741edbd739b6161de49
endiczek <endiczek@gmail.com>
2021-04-29 23:15:25 +0200

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is in the air!\n";
-print "And now will be in the app as well";
+print "Hello, World! php version is " . PHP_VERSION . "<br>\n";
+print "A change is in the air!<br>\n";
+print "And now will be in the app as well\n";
 ?>
